8b03bb02c2c058b3c0550ebd2f8581e0b79faa97
jominkook <whalsrnraos@naver.com>
2024-11-06 19:50:25 +0900

    스프링부트3 백엔드 개발자 되기 4일차 JPA,ORM

---
+++ src/test/java/com/example/demo/MemberRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.example.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getAllMembers() {
+
+/*        //when
+        List<Member> members = memberRepository.findAll();
+
+        //then
+        assertThat(members.size()).isEqualTo(3);*/
+
+        //when
+        Member member = memberRepository.findById(2L).get();
+
+        //then
+        assertThat(member.getName()).isEqualTo("B");
+    }
+
+    @Test
+    void saveMember() {
+        //given
+        Member member = new Member(1L,"A");
+
+        //when
+        memberRepository.save(member);
+
+        //then
+        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }
+
+    @Test
+    void saveMembers() {
+        //given
+        List<Member> members = List.of(new Member(2L,"B"),
+                new Member(3L,"C"));
+
+        //when
+        memberRepository.saveAll(members);
+
+        //then
+        assertThat(memberRepository.findAll().size()).isEqualTo(2);
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteMember() {
+
+        //when
+        memberRepository.deleteById(2L);
+
+        //then
+        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteAll() {
+
+        //when
+        memberRepository.deleteAll();
+
+        //then
+        assertThat(memberRepository.findAll().size()).isZero();
+    }
+
+}
