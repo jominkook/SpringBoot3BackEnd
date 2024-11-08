e8d1f6555b2804a53a99ba337bf40568abe87072
jominkook <whalsrnraos@naver.com>
2024-11-08 17:30:58 +0900

    스프링부트3 백엔드 개발자 되기 6일차 블로그만들기-2

---
+++ src/main/java/com/example/demo/ArticleRequest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ArticleRequest {
+    private String title;
+
+    private String content;
+
+    public Article toEntity() {
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
