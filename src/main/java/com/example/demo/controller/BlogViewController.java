3be9287296538a55e7a0dd68a77d10d49d691a51
jominkook <whalsrnraos@naver.com>
2024-11-13 18:35:19 +0900

    스프링부트3 백엔드 개발자 되기 7일차 타임리프 실습-1

---
+++ src/main/java/com/example/demo/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Article;
+import com.example.demo.dto.ArticleListViewResponse;
+import com.example.demo.dto.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
